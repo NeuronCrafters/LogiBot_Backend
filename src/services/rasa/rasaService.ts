6ee6c7244ca562230ab1147799a0a758dc5c16f7
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-01-16 16:26:09 -0300

    interacao com rasa feita com sucesso

--- src/services/rasa/rasaService.ts
+++ src/services/rasa/rasaService.ts
@@ -15,26 +15,30 @@ class RasaService {
 
   async sendMessageToSAEL({ sender, message }: RasaMessageRequest) {
     try {
-      const response = await axios.post(this.rasaUrl, {
-        sender,
-        message,
-      });
+      const response = await axios.post(this.rasaUrl, { sender, message });
 
       if (!response.data || response.data.length === 0) {
-        throw new AppError("Nenhuma resposta do Rasa", 502);
+        throw new AppError("Nenhuma resposta do Rasa foi recebida.", 502);
       }
 
       return response.data;
     } catch (error: any) {
+
+      console.error("Erro no serviço Rasa:", error);
+
       if (error.response) {
-        throw new AppError(`Erro do Rasa: ${error.response.data.message}`, error.response.status);
+        throw new AppError(
+          `Erro do Rasa: ${error.response.data.message || error.response.statusText}`,
+          error.response.status
+        );
       } else if (error.request) {
-        throw new AppError("Falha na comunicação com o Rasa", 503);
+        throw new AppError("Falha na comunicação com o Rasa. Nenhuma resposta recebida.", 503);
       } else {
-        throw new AppError("Erro ao processar mensagem no Rasa", 500);
+        throw new AppError(`Erro desconhecido ao processar mensagem no Rasa: ${error.message}`, 500);
       }
     }
   }
+
 }
 
 export { RasaService };

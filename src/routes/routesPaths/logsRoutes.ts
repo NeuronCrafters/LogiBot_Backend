7a22ad3db5f15ab76b500b7020fd1f759ce7e2d9
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-03-25 11:27:45 -0300

    criado service e controller para logs de class

--- src/routes/routesPaths/logsRoutes.ts
+++ src/routes/routesPaths/logsRoutes.ts
@@ -3,20 +3,25 @@ import { LogUserController } from "../../controllers/Logs/LogUserController";
 import { LogDisciplineController } from "../../controllers/Logs/LogDisciplineController";
 import { LogCourseController } from "../../controllers/Logs/LogCourseController";
 import { isPermissions } from "../../middlewares/isPermissions/isPermissions";
+import { LogClassController } from "../../controllers/Logs/LogClassController";
 
 const logRoutes = Router();
 
 const userController = new LogUserController();
 const disciplineController = new LogDisciplineController();
 const courseController = new LogCourseController();
+const classController = new LogClassController();
 
-// logs de um usuário (admin, coordenador ou o próprio aluno)
+// logs de um usuário (admin, coordenador e aluno)
 logRoutes.get("/user/:userId", isPermissions.isAdminProfessorOrCoordinator(), userController.getUserLogs.bind(userController));
+
+// logs de uma turma (admin e coordenador)
+logRoutes.get("/class/:classId", isPermissions.isAdminOrCoordinator(), classController.getClassLogs.bind(classController));
 
 // logs de uma disciplina (admin, coordenador ou professor da disciplina)
 logRoutes.get("/discipline/:disciplineId", isPermissions.isAdminProfessorOrCoordinator(), disciplineController.getDisciplineLogs.bind(disciplineController));
 
-// logs de um curso (apenas admin e coordenador)
+// logs de um curso (admin e coordenador)
 logRoutes.get("/course/:courseId", isPermissions.isAdminOrCoordinator(), courseController.getCourseLogs.bind(courseController));
 
 export { logRoutes };

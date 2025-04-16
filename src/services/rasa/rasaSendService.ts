a0e7f1d611010ba11daa3c6ca64af1dcb4c5720c
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-04-16 08:37:26 -0300

    ...

--- src/services/rasa/rasaSendService.ts
+++ src/services/rasa/rasaSendService.ts
@@ -1,21 +1,20 @@
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const rasa_send = process.env.RASA;
+const rasa_send: string = process.env.RASA;
 if (!rasa_send) {
   console.error("Variável de ambiente RASA não definida.");
 }
 
-async function rasaSendService(message: string, sender: string) {
+async function rasaSendService(message: string, sender: string): Promise<any> {
   if (!sender) {
     throw new Error("Sender (usuário) não pode ser indefinido.");
   }
 
   try {
-
-    const response = await axios.post(rasa_send, { sender, message });
+    const response: AxiosResponse<any, any> = await axios.post(rasa_send, { sender, message });
 
     if (!response.data || response.data.length === 0) {
       console.warn("[RasaServiceSend] Nenhuma resposta do Rasa.");
@@ -23,6 +22,7 @@ async function rasaSendService(message: string, sender: string) {
     }
 
     return response.data;
+
   } catch (error: any) {
     if (error.response) {
       console.error("[RasaServiceSend] Erro ao conectar ao Rasa. Status:", error.response.status, "Dados:", error.response.data);

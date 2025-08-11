3d05a94665df2a5f0cb6833aa3d73a07e5a4fedf
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-08-11 09:33:06 -0300

    ajustes na migration

--- src/migrations/add-status-to-users.ts
+++ src/migrations/add-status-to-users.ts
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import path from 'path';
 import { User } from '../models/User'
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config();
 
 async function runMigration() {
   const MONGO_URI = process.env.MONGO_URI as string;
@@ -20,15 +19,10 @@ async function runMigration() {
 
     console.log('🚀 Iniciando a migração para adicionar o campo "status"...');
 
-    // 1. O Filtro: Encontrar todos os usuários que NÃO possuem o campo 'status'.
-    // Isso torna o script seguro para ser executado múltiplas vezes (idempotente).
     const filter = { status: { $exists: false } };
 
-    // 2. A Atualização: Definir o campo 'status' como 'active'.
-    // Usamos o operador $set para adicionar/modificar o campo.
     const update = { $set: { status: 'active' } };
 
-    // 3. A Execução: Usar updateMany para aplicar a atualização em todos os documentos encontrados.
     const result = await User.updateMany(filter, update);
 
     console.log('✨ Migração concluída!');
@@ -38,11 +32,9 @@ async function runMigration() {
   } catch (error) {
     console.error('🔥 Ocorreu um erro durante a migração:', error);
   } finally {
-    // Garante que a conexão com o banco de dados será sempre fechada
     console.log('🔌 Desconectando do MongoDB...');
     await mongoose.disconnect();
   }
 }
 
-// Executa a função principal da migração
 runMigration();

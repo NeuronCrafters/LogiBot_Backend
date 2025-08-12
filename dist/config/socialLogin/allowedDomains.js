59d759966e767d81d622e5645b0b867dfd842657
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-08-11 23:04:14 -0300

    remoção dos arquivos de dist

--- dist/config/socialLogin/allowedDomains.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allowedDomains = void 0;
-exports.allowedDomains = ['unifesspa.edu.br', 'ufpa.com.br', 'cin.ufpe.br', 'unb.br'];
--- dist/services/rasa/ActionService/getGabaritoService.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getGabaritoService = getGabaritoService;
-function getGabaritoService(session) {
-    return {
-        answer_keys: session.lastAnswerKeys,
-        assunto: session.lastSubject,
-    };
-}
